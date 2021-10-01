6352c3e51dd80335d829dec0b003495a7a0039b6
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-10-01 13:18:48 -0700

    Add create route for /vendors/{id}/orders

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -33,5 +33,26 @@ namespace VendorTracker.Controllers
       Console.WriteLine($"Vendor name: {selectedVendor.Name}");
       return View(selectedVendor); 
     }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, int number, string description)
+    {
+      Console.WriteLine($"In Create order controller route");
+      //get the vendor, create the order, add the order, pass vendor to Show
+
+      Vendor vendorToAddTo = Vendor.Find(vendorId);
+
+      Order newOrder = new Order(number, description);
+      // newOrder.Date = date;
+
+      vendorToAddTo.AddOrder(newOrder);
+
+      Console.WriteLine($"Order num of items: {newOrder.NumItems}");
+      Console.WriteLine($"Order desc: {newOrder.Description}");
+      Console.WriteLine($"Order price: {newOrder.Price}");
+      Console.WriteLine($"Order date: {newOrder.Date}");
+      
+      return View("Show", vendorToAddTo);
+    }
   }
 }
