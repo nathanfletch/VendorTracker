e463f9eba35ca93d554e58a3b7d289dc780dc5eb
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-10-01 11:36:13 -0700

    Add route for vendor creation

---
+++ VendorTracker/Controllers/OrdersController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorTracker.Models;
+using System;
+using System.Collections.Generic;
+
+namespace VendorTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+    // [HttpGet("/")]
+    // public ActionResult Index() { 
+    //   return View(); 
+    // }
+  }
+}
---
+++ VendorTracker/Controllers/VendorsController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorTracker.Models;
+using System;
+using System.Collections.Generic;
+
+namespace VendorTracker.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index() { 
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors); 
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New() { 
+      return View(); 
+    }
+    
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName) { 
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index"); 
+    }
+  }
+}
