a4e7099add761b58cf2c84869008ba519e605b6b
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-10-01 08:50:07 -0700

    Add initial files

---
+++ VendorTracker/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VendorTracker.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index() { 
+      return View(); 
+    }
+  }
+}
